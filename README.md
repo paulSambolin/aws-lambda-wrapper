f6bf5cb5213efef77ce045263656b1475a7da623
paulSambolin <paulsambolin@gmail.com>
2017-01-25 13:16:07 -0600

    initial commit of lambda-low-wrapper

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-This is wrapper around node lambda used to simply calls to lambda functions.
+## Lambda Logger
+This npm package enables nodejs to estimate the time it takes a call to a lambda function to return.
+
+- Get the time immediately before calling a lambda function
+- Get the time immediately after the call returns
+- Log the differene to a Log group with a naming convention of `/metric/lambda/<caller-name>/<invoked-name>`
+
+## When using serverless
+- Serverless automatically appends the stage variable to lambda functions when deploying to aws
+- It is suggested you add the stage variable as an environment variable  for your lamdba functions.  This allows you to dynamically piece together the correct funciton name for the stage being tested
+
+```javascript
+var funcitonName = 'helloworld' + '-' + process.env.STAGE;
+```
+
+
+
+
+
+
+
+## Update the Security template
+- Give functions permission to:
+  - Create log group
+  - put cloudwatch metrics
+
+## Update the application template
+- Add new npm package
+
+
+
+
+## Usage
+```javascript
+// Initialize lambda logger
+var Lambda = require('lambda-logger');
+
+exports.handler = (event, context, callback) => {
+    // Pass the context object to lambda wrapper
+    var lambda = new Lambda(context);
+}
+
+// Invoke function
+var request = {
+    ProcessId: pid,
+    TranactionId: tid
+};
+lambda.invoke('Helloworld', request);
+
+// Micro-services should return:
+{
+    RequestId: rid,
+    FunctionVersion: fv
+    //additional data??
+}
+```
--- index.js
+++ index.js
@@ -3,43 +3,87 @@
 const AWS = require('aws-sdk');
 
 class Lambda {
-    constructor(config) {
-        // const params = {
-        //     region: config.region
-        // };
-
-        // if (config.profile) {
-        //     params.credentials = new AWS.SharedIniFileCredentials({
-        //         profile: config.profile
-        //     });
-        // }
-
-        // this.accountId = config.accountId;
-        // this.region = config.region;
-        this.lambda = new AWS.Lambda(params);
+    constructor(context) {
+        this.context = context;
+        this.lambda = new AWS.Lambda();
+        this.cloudwatch = new AWS.CloudWatch();
+        this.cloudwatchlogs = new AWS.CloudWatchLogs();
     }
 
-    invoke(name, qualifier, eventType, payload) {
+    invoke(functionName, event) {
+        return new Promise((resolve, reject) => {
+            var caller = {
+                FunctionName: this.context.fucntionName,
+                RequestId: this.context.awsRequestId
+                //The ARN used to invoke this function (not sure of it's use/value)
+                //,InvokedFucntionArn: this.context.invokedFunctionArn
+            };
 
-        const fnArn = `arn:aws:lambda:${this.region}:${this.accountId}:function:${name}`;
-        const deferred = Q.defer();
-        const params = {
-            FunctionName: fnArn,
-            InvocationType: eventType,
-            Payload: JSON.stringify(payload),
-            Qualifier: qualifier
-        };
+            event.caller = caller;
+            const request = {
+                FunctionName: functionName,
+                InvocationType: 'RequestResponse',
+                Payload: JSON.stringify(event)
+            };
 
-        this.lambda.invoke(params, (err, data) => {
-            if (err) {
-                deferred.reject(err);
-            } else {
-                deferred.resolve(data);
-            }
+            var startTime = Date.now();
+            this.lambda.invoke(request, (err, response) => {
+                var endTime = Date.now();
+
+                if (err) {
+                    reject(err);
+                } else {
+                    // Log the call data into the Controller's log group
+                    var log = {
+                        request: request,
+                        response: response,
+                        duration: duration,
+                        function:  request.FunctionName,
+                        version: response.FunctionVersion
+                    };
+                    console.log(JSON.stringify(log));
+                    
+                    // Put duration metric in unique Cloudwatch metric <controller>/<service>
+                    var pmParams = {
+                        MetricData: [
+                            {
+                                MetricName: this.context.fucntionName+'/'+functionName,
+                                Timestamp: new Date(),
+                                Unit: 'Milliseconds',
+                                Value: duration
+                            },
+                        ],
+                        Namespace: 'CUSTOM/lambda'
+                    };
+
+                    putMetricData(pmParams)
+                    .then(createLogGroup)
+                    .then(() => {
+                        resolve(response);
+                    });                    
+                }
+            });
         });
-
-        return deferred.promise;
     }
 }
 
 module.exports = Lambda;
+
+function putMetricData(metric) {
+    return new Promise((resolve) => {
+        cloudwatch.putMetricData(metric, function(err, data) {
+            resolve(metric.MetricData[0].MetricName);
+        });
+    });
+}
+
+function createLogGroup(logGroupName) {
+    return new Promise((resolve) => {
+        var lgParams = {
+            logGroupName: logGroupName
+        };
+        cloudwatchlogs.createLogGroup(lgParams, function(err, data) {
+            resolve(resource);
+        });
+    });
+}
